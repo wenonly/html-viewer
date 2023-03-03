ed585f6eeb31ab6096b58d281d8e93149b8802de
陶文 <taowen@sobey.com>
2023-03-03 13:59:24 +0800

    chore: 添加lib打包分析命令

--- configs/vite.lib.config.ts
+++ configs/vite.lib.config.ts
@@ -1,6 +1,7 @@
 import vue from '@vitejs/plugin-vue';
 import path from 'path';
 import { defineConfig } from 'vite';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
       name: 'HtmlViewer',
     },
     rollupOptions: {
+      plugins: process.env.ANALYZE ? [visualizer({ open: true })] : undefined,
       input: ['src/lib/index.ts'],
       output: [
         {
